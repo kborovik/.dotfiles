7733567f01f26481d0582dc56ea1e0660f0eaad4
Konstantin Borovik <github@lab5.ca>
2026-04-23 09:01:24 -0400

    feat(fish): bind alt-backspace to backward-kill-path-component

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -4,5 +4,7 @@ function fish_user_key_bindings
     bind -M insert ctrl-p up-line
     bind -M insert ctrl-n down-line
     bind -M insert ctrl-backspace backward-kill-token
+    bind -M insert alt-backspace backward-kill-path-component
     bind ctrl-backspace backward-kill-token
+    bind alt-backspace backward-kill-path-component
 end

350cdccf6c72ad551f8aee1131d138fb613073f7
Konstantin Borovik <github@lab5.ca>
2026-04-23 08:51:41 -0400

    chore(config): update gitignore, fish keybindings, and zed settings
    - Add .firecrawl/ to .gitignore and reorganize ignore patterns
    - Bind ctrl-backspace to backward-kill-token in fish vi insert mode
    - Zed: open CLI files in existing window, move panels to right/left docks,
      switch default agent profile to "write" and model to gpt-5.2

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -3,4 +3,6 @@ function fish_user_key_bindings
     bind -M insert ctrl-r history-pager
     bind -M insert ctrl-p up-line
     bind -M insert ctrl-n down-line
+    bind -M insert ctrl-backspace backward-kill-token
+    bind ctrl-backspace backward-kill-token
 end
